125cd628955e010e9525ed111e092dbdb760fd25
Konabur <shaikhiev2014@gmail.com>
2024-01-15 21:51:33 +0300

    add Event and Organizations models, edit serializers for User

--- core/models.py
+++ core/models.py
@@ -1,7 +1,23 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)  # Обязательное поле для хранения пароля
+
+
+class Organization(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    address = models.CharField(max_length=255)
+    postcode = models.CharField(max_length=10)
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    organizations = models.ManyToManyField(Organization)
+    image = models.ImageField(upload_to='event_images/', null=True, blank=True)
+    date = models.DateTimeField()
--- core/serializers.py
+++ core/serializers.py
@@ -3,17 +3,19 @@ from django.core.exceptions import ValidationError
 from rest_framework import serializers
 from .models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'phone_number', 'password')
+        fields = ('id', 'email', 'phone_number', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
-    
-    def validate_email(self, value):
+
+    @staticmethod
+    def validate_email(value):
         """
         Валидация email.
         """
@@ -21,7 +23,8 @@ class UserSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Email уже используется.")
         return value
 
-    def validate_password(self, value):
+    @staticmethod
+    def validate_password(value):
         """
         Валидация пароля.
         """
