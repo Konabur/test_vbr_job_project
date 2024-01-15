b19c577d72e52e1338e954910ae762545115eeb2
Konabur <shaikhiev2014@gmail.com>
2024-01-15 20:36:09 +0300

    add email and password validation

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 from .models import User
 
@@ -10,3 +12,21 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+    
+    def validate_email(self, value):
+        """
+        Валидация email.
+        """
+        if User.objects.filter(email__iexact=value).exists():
+            raise serializers.ValidationError("Email уже используется.")
+        return value
+
+    def validate_password(self, value):
+        """
+        Валидация пароля.
+        """
+        try:
+            validate_password(value)
+        except ValidationError as e:
+            raise serializers.ValidationError(str(e))
+        return value
