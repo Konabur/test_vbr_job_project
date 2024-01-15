221c79943ad13d17187ca8db820de43b25a04cae
Konabur <shaikhiev2014@gmail.com>
2024-01-15 19:38:52 +0300

    add simple jwt

--- orgEventsApi/settings.py
+++ orgEventsApi/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt'
     'users',
 ]
 
@@ -123,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
