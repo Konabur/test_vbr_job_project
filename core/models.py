a7669cc6e66f75291726e07f83ef9679a64de8fb
Konabur <shaikhiev2014@gmail.com>
2024-01-16 04:07:02 +0300

    clearing models

--- core/models.py
+++ core/models.py
@@ -1,4 +1,3 @@
-# from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -6,7 +5,6 @@ class User(models.Model):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)  # Обязательное поле для хранения пароля
-    organizations = models.ManyToManyField('Organization', related_name='members', blank=True)
 
 
 class Organization(models.Model):
@@ -14,12 +12,12 @@ class Organization(models.Model):
     description = models.TextField()
     address = models.CharField(max_length=255)
     postcode = models.CharField(max_length=10)
-    members = models.ManyToManyField('users.User', related_name='organizations', blank=True)
+    members = models.ManyToManyField(User, related_name='organizations', blank=True)
 
 
 class Event(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    organizations = models.ManyToManyField('Organization', related_name='events')
+    organizations = models.ManyToManyField(Organization, related_name='events')
     image = models.ImageField(upload_to='event_images/', null=True, blank=True)
     date = models.DateTimeField()
